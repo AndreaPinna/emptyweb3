ca30abc9e94820ff38994c3cd4de803a7f0eb68b
AndreaPinna <50105901+AndreaPinna@users.noreply.github.com>
2019-05-25 11:46:52 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Documentation
+// https://github.com/ethereum/wiki/wiki/JavaScript-API
+
+//1. Caricare web3 e lanciare la funzione startApp (che lavora come una main)
+window.addEventListener('load', function () {
+
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        console.log('No web3? You should consider trying MetaMask!')
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+
+});
